7e719d2d5b157b311f8028088bfdd86b2f17e80a
Your Name <matthewsoto85@gmail.com>
2023-07-18 22:55:05 -0400

    frontend update

--- frontend/src/Frontend/Home.js
+++ frontend/src/Frontend/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <main className='home'>
         <h1>Welcome</h1>
-        <p>Customize a personal pc set up with the suggestions I've handpicked</p>
-        <p> this is meant to be for the general people as the suggestions will fit perfectly for the majority, so you aren't endlessly scrolling the internet for what to choose. </p>
+        <p>Customize a personal pc set up with the suggestions I've handpicked.</p>
+        <p>This is meant to be for the general people as the suggestions will fit perfectly for the majority, so you aren't endlessly scrolling the internet for what to choose.</p>
         <img src={img} alt='pc' />
     </main>
   )
